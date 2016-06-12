09bb813d09c3bb753e974ddc1761328f1427dca4
Tushar <tusharm567@gmail.com>
2016-06-13 02:22:49 +0530

    First Commit

---
+++ js/json-helper.js
@@ -0,0 +1,29 @@
+var preDate = "<li> <div class='collapsible-header active waves-effect waves-light grey darken-2 white-text'> <i class='mega-octicon octicon-calendar'></i> <b>";
+
+var postDate = "</b> </div>";
+
+var headPosts = "<div class='collapsible-body grey lighten-5'> <ul class='details'>"
+
+var preContent = "<li><b>";
+
+var postContent = "</li>";
+
+var endPosts = "<br/></ul></div></li>";
+
+$.get('posts.json', function(data) {
+	var posts = $.parseJSON( data );
+	var content = "";
+	$.each(posts, function(i, item) {
+		content += (preDate+item.date+postDate);
+		content += (headPosts);
+
+		var postsString = "";
+		$.each(item.entries, function(i, item) {
+			content += preContent + item + postContent;
+		});
+		content += endPosts;
+		console.log("#############Adding \n"+content );
+		$('#postsContainer').append(content);
+		content = "";
+	});
+}); 
